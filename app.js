6e01552b37a62ffd1eb70337bdef6fbbc9fdf54c
Jeevan Thapa <jeevanthapa1987@yahoo.com>
2022-11-22 09:50:31 -0500

    first inquirer file

--- app.js
+++ app.js
@@ -1,8 +1,4 @@
-
- 
-
-
-
+/*
   const fs = require('fs');
   const generatePage = require('./src/page-template.js');
 
@@ -17,3 +13,30 @@ fs.writeFile('./index.html', generatePage(name, github), err => {
 
   console.log('Portfolio complete! Check out index.html to see the output!');
 });
+
+*/
+//const inquirer = require('inquirer');
+
+const inquirer = require('inquirer');
+inquirer 
+.prompt ([
+{
+  type:'input',
+  name:'name',
+  message:'What is your name ?'
+}  
+])
+.then(answers => console.log(answers));
+
+/*
+const fs = require('fs');
+const generatePage = require('./src/page-template.js');
+
+const pageHTML = generatePage(name, github);
+
+fs.writeFile('./index.html', pageHTML, err => {
+  if (err) throw err;
+
+  console.log('Portfolio complete! Check out index.html to see the output!');
+});
+*/
---
+++ server.js
@@ -0,0 +1,9 @@
+inquirer
+  .prompt([
+    {
+      type: 'input',
+      name: 'name',
+      message: 'What is your name?'
+    }
+  ])
+  .then(answers => console.log(answers));
